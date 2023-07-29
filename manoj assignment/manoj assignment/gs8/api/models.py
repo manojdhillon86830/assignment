d14a1decd2bc6764dfacfc0b73dfc3bb78d4ccbd
Manoj Dhillon <manojsingh86830@gmail.com>
2023-07-29 13:26:46 +0530

    new uid field

--- manoj assignment/manoj assignment/gs8/api/models.py
+++ manoj assignment/manoj assignment/gs8/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 "This is the testing for new branch "
 # Create your models here.
 class Student(models.Model):
+    first_name = models.CharField(max_length=20, null=True, blank=True)
     name = models.CharField(max_length=100)
     roll = models.IntegerField()
     city = models.CharField(max_length=100)
